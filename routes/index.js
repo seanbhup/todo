5dd410aed76a5154c7ef0de56d5c7a95b1cc2891
Sean Bhupathi <seanbhupathi@gmail.com>
2017-02-07 13:05:58 -0500

    queries working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,80 @@
 var express = require('express');
 var router = express.Router();
+var config = require("../config/config");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : config.host,
+    user     : config.userName,
+    password : config.password,
+    database : config.database
+});
+
+// After this line, we are connected to mysql
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // taskArray = [];
+    var selectQuery = "Select * FROM tasks";
+    connection.query(selectQuery, (error,results,field)=>{
+        // res.json(results);
+        res.render('index', {taskArray: results});
+    });
+});
+
+
+router.post("/addNew", (req,res,next)=>{
+    var newTask = req.body.newTaskString;
+    var taskDate = req.body.newTaskDate;
+    var insertQuery = "INSERT INTO tasks (task_name, task_date) VALUES ('"+newTask+"','"+taskDate+"')";
+
+    connection.query(insertQuery, (error,results,field)=>{
+        if (error) throw error;
+        res.redirect("/");
+    });
+
+});
+// ---------------EDIT GET-----------------
+router.get("/edit/:id", (req,res,next)=>{
+    var selectQuery = "SELECT * from tasks WHERE id="+req.params.id;
+
+    connection.query(selectQuery, (error,results,fields)=>{
+        var days = results[0].task_date.getDate();
+        if(days < 10){
+            days = "0"+days;
+        }
+        var months = results[0].task_date.getMonth() + 1;
+        if(months < 10){
+            months = "0"+months;
+        }
+        var years = results[0].task_date.getFullYear();
+        if(years < 10){
+            years = "0"+years;
+        }
+        var mysqlDate = years + "-" + months + "-" + days;
+        results[0].task_date = mysqlDate;
+
+        res.render("edit", {task:results[0]});
+    })
+});
+// ---------------EDIT POST-----------------
+router.post("/edit/:id", (req,res,next)=>{
+    var id = req.params.id;
+    var newTask = req.body.newTaskString;
+    var taskDate = req.body.newTaskDate;
+    var updateQuery = "UPDATE tasks SET task_name='"+newTask+"', task_date='"+taskDate+"'WHERE ID="+id;
+
+    res.send(updateQuery);
+});
+// ---------------DELETE GET-----------------
+router.get("/delete/:id", (req,res,next)=>{
+    var selectQuery = "SELECT * from tasks WHERE id="+req.params.id;
+
+
+});
+// ---------------DELETE POST-----------------
+router.post("/delete/:id", (req,res,next)=>{
+    // res.send(req.params.id);
 });
 
 module.exports = router;
